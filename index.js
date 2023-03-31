b8daf0241f9f9cc121aaf94d373529eb24b65568
Swayam <95397970+schlechter-afk@users.noreply.github.com>
2023-03-31 12:15:28 +0530

    mime type error fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
 // arrays to pass to html
